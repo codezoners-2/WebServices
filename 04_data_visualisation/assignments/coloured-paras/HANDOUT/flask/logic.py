9014fba0f6b220ef793730585e2bc419405e2b28
Nick (Yedikule) <nick@cassiel.com>
2015-04-26 16:34:04 +0100

    Purge.

--- 04_data_visualisation/assignments/coloured-paras/HANDOUT/flask/logic.py
+++
@@ -1,9 +0,0 @@
-import sys
-import functools
-
-def filesToDict(listOfFiles):
-    '''
-    >>> sorted(filesToDict(['test/testRead1.txt','test/testRead2.txt']).items())
-    [('test/testRead1.txt', ['one', 'two']), ('test/testRead2.txt', ['three', 'four', 'three'])]
-    '''
-    return { f: open(f).read().splitlines() for f in listOfFiles }
